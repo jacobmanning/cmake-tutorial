f468d699002747345ab4a7dd48e6fad03c3298eb
Jacob Manning <jacobmanning@pitt.edu>
2018-05-26 17:03:36 -0400

    Remove old comments

--- Test3/buildit.sh
+++ Test3/buildit.sh
@@ -4,8 +4,6 @@ original_directory=$(pwd)
 
 # Create build directory
 mkdir build && cd build
-#mkdir bin
-#mkdir include
 
 # Configure with CMake, modify to `-DVERBOSE=OFF` or leave
 # default value (OFF) by removing the definition if you want
